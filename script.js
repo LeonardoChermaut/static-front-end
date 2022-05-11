3403a33b7c5c7b5155adb5d0638d5c80395f481b
Leoanrdo Chermaut <chermauts@hotmail.com>
2022-05-11 17:23:35 -0300

    ola-mundo

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    getUsers();
+}
+
+function getUsers() {
+    fetch("https://randomuser.me/api/?results=5").then(resultado => {
+        return resultado.json();
+    }).then(response => {
+        let cards = document.getElementsByClassName('card')
+
+        response.results.forEach((user, index) => {
+            let card = cards[index]
+            let photo = card.getElementsByClassName('card-img-top')[0];
+            photo.src = user.picture.large;
+
+            let names = card.getElementsByClassName('card-title')[0];
+            names.innerHTML = `${user.name.first} ${user.name.last}`
+
+            let description = card.getElementsByClassName('card-text')[0];
+            description.innerHTML = ` ${user.location.city}<br>${user.location.country}<br>${user.location.timezone.description}`
+
+        });
+    })
+}
